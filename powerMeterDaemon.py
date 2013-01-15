dc6b37c0e6baaf67f18d5a2f94c64a7bb6d032a0
madmaze <matthias.a.lee@gmail.com>
2013-01-14 22:23:18 -0500

    added switch for turning plotting on and off:

--- powerMeterDaemon.py
+++ powerMeterDaemon.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python
 
-# Math and Serial imports
+# Misc imports
 import numpy as np
 import math
 import serial
+from threading import Timer 
 
-# GRAPHING imports
-import wx
-import matplotlib
-matplotlib.use('WXAgg') # do this before importing pylab
-from pylab import *
+
+# Globals.. still needed?	
+totalVolt=0.0
+totalAmp=0.0
+tcnt=0
+avgwattdataidx=0
+plotGraph=True
+
+if plotGraph:
+	# GRAPHING imports
+	import wx
+	import matplotlib
+	matplotlib.use('WXAgg') # do this before importing pylab
+	from pylab import *
 
 def parsePacket(packet):
 	data=[]
@@ -29,14 +39,8 @@ def parsePacket(packet):
 		print "Skipped packet:",packet
 		
 	return data
-
-# Globals.. still needed?	
-totalVolt=0.0
-totalAmp=0.0
-tcnt=0
-avgwattdataidx=0
-
-def graphIt(data):
+	
+def processData(data):
 	N_samples = 160
 	WaveLength = (74*2)+1 # we want 2 waves to even out random noise.. 149 samples is good
 	VoltSense = 1
@@ -49,7 +53,8 @@ def graphIt(data):
 	CURRENTNORM = 156  # conversion to amperes from ADC
 	VOLTNORM = .479 # conversion to volts from ADC
 	
-	fig.canvas.draw()
+	if plotGraph:
+		fig.canvas.draw()
 	
 	# init empty arrays
 	voltagedata=[0]*N_samples
@@ -68,7 +73,7 @@ def graphIt(data):
         	ave1 += voltagedata[i]
         	ave2 += ampdata[i]
         	
-        print "V/A>>>>>>>>>>", ave1/WaveLength, ave2/WaveLength, WaveLength
+        #print "V/A>>>>>>>>>>", ave1/WaveLength, ave2/WaveLength, WaveLength
 
 	vave=0.0
         for i in range(len(voltagedata)):
@@ -124,22 +129,25 @@ def graphIt(data):
                 avgwattdata[i] = 0
             avgwattdataidx = len(avgwattdata) - tenpercent
             
-            
-	wattusageline.set_ydata(avgwattdata)
-        voltagewatchline.set_ydata(voltagedata)
-        ampwatchline.set_ydata(ampdata)
+        if plotGraph:
+		wattusageline.set_ydata(avgwattdata)
+		voltagewatchline.set_ydata(voltagedata)
+		ampwatchline.set_ydata(ampdata)
         
         # Debug Info
-	print "ave Amp:", aave, aave/149
+	print "\nave Amp:", aave, aave/149
 	print "ave Volt:", vave, vave/149, vmin, vmax
-	print "rms/trueRMS Volt", vmax/sqrt(2), vRMS
+	print "rms/trueRMS Volt", vmax/math.sqrt(2), vRMS
 	print "ave Watt:", wattAve
 	if wattAve > 100:
 		plt.savefig("test.png")
 		print "I think we may have come across an odd spike.. exiting and saving plot"
 		exit()
         
-def readData(event):
+def readDataEvent(event):
+	readData()
+		
+def readData():
 	global totalVolt
         global totalAmp
 	global tcnt
@@ -147,46 +155,55 @@ def readData(event):
 		p = s.readline().strip()
 		data = parsePacket(p)
 		if data != []:
-			graphIt(data)
+			processData(data)
+			
+	if plotGraph is False:
+		Timer(0.25,readData).start()
 
-# Setup code mostly borrowed from Adafruit's wattagePlotter.py 
-# Create an animated graph
-fig = plt.figure()
-
-# with three subplots: line voltage/current, watts and watthr
-wattusage = fig.add_subplot(211)
-mainswatch = fig.add_subplot(212)
-
-# data that we keep track of, the average watt usage as sent in
+# average Watt data
 avgwattdata = [0] * 1800 # zero out all the data to start
-avgwattdataidx = 0 # which point in the array we're entering new data
 
-# The watt subplot
-watt_t = np.arange(0, len(avgwattdata), 1)
-wattusageline, = wattusage.plot(watt_t, avgwattdata)
-wattusage.set_ylabel('Watts')
-wattusage.set_ylim(0, 100)
-    
-# the mains voltage and current level subplot
-mains_t = np.arange(0, 160, 1)
-voltagewatchline, = mainswatch.plot(mains_t, [0] * 160, color='blue')
-mainswatch.set_ylabel('Volts')
-mainswatch.set_xlabel('Sample #')
-mainswatch.set_ylim(-200, 200)
-
-# make a second axies for amp data
-mainsampwatcher = mainswatch.twinx()
-ampwatchline, = mainsampwatcher.plot(mains_t, [0] * 160, color='green')
-mainsampwatcher.set_ylabel('Amps')
-mainsampwatcher.set_ylim(-15, 15)
-
-# and a legend for both of them
-legend((voltagewatchline, ampwatchline), ('volts', 'amps'))
+if plotGraph:
+	# Setup code mostly borrowed from Adafruit's wattagePlotter.py 
+	# Create an animated graph
+	fig = plt.figure()
+	
+	# with three subplots: line voltage/current, watts and watthr
+	wattusage = fig.add_subplot(211)
+	mainswatch = fig.add_subplot(212)
+	
+	avgwattdataidx = 0 # which point in the array we're entering new data
+	
+	# The watt subplot
+	watt_t = np.arange(0, len(avgwattdata), 1)
+	wattusageline, = wattusage.plot(watt_t, avgwattdata)
+	wattusage.set_ylabel('Watts')
+	wattusage.set_ylim(0, 100)
+	    
+	# the mains voltage and current level subplot
+	mains_t = np.arange(0, 160, 1)
+	voltagewatchline, = mainswatch.plot(mains_t, [0] * 160, color='blue')
+	mainswatch.set_ylabel('Volts')
+	mainswatch.set_xlabel('Sample #')
+	mainswatch.set_ylim(-200, 200)
+	
+	# make a second axies for amp data
+	mainsampwatcher = mainswatch.twinx()
+	ampwatchline, = mainsampwatcher.plot(mains_t, [0] * 160, color='green')
+	mainsampwatcher.set_ylabel('Amps')
+	mainsampwatcher.set_ylim(-15, 15)
+	
+	# and a legend for both of them
+	legend((voltagewatchline, ampwatchline), ('volts', 'amps'))
 
 s = serial.Serial(port='/dev/ttyUSB0',baudrate=115200)
 s.open()
 
-timer = wx.Timer(wx.GetApp(), -1)
-timer.Start(250)        # run an in every 'n' milli-seconds
-wx.GetApp().Bind(wx.EVT_TIMER, readData)
-plt.show()
+if plotGraph:
+	print "Setting up graphs.."
+	timer = wx.Timer(wx.GetApp(), -1)
+	timer.Start(250)        # run an in every 'n' milli-seconds
+	wx.GetApp().Bind(wx.EVT_TIMER, readDataEvent)
+	plt.show()
+else:
+	readData()
